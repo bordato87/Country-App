9e92281923c1c9a8c0213c905b982b1e6c180dd8
bordato87 <bordato87@gmail.com>
2021-08-04 17:43:24 -0300

    models

---
+++ api/src/models/Activity.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('activity', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        dificulty: {
+            type: DataTypes.ENUM(['1', '2', '3', '4', '5']),
+            allowNull: false
+        },
+
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    });
+};
